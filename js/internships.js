4df4d8439c4d9ea7af3a56ee8d68d7f96a0b331e
maxios <maxios.code@gmail.com>
2017-01-24 23:47:44 +0200

    adding background parallax to internships page for the leftside

--- js/internships.js
+++ js/internships.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
 
     $('.nav-btn').css('color', 'white');
 
+    $('.field').click(function(){
+      $('#leftside').css('filter', 'brightness(0.5)');
+    });
+    $('#leftside').click(function(){
+      $('#leftside').css('filter', 'brightness(1)');
+    })
 });
--- js/why.js
+++ js/why.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
 
   $('.landing').css('height', $(window).height());
   $('.nav-btn').css('color', 'white');
+  $('#logo_text').css('color', 'white');
 
 });
