f78124a5eed756d55488cfbe08ebc1ba42daef33
maxios <maxios.code@gmail.com>
2017-01-28 00:07:17 +0200

    join.html created

--- js/internships.js
+++ js/internships.js
@@ -1,11 +1,11 @@
+var nav_color = 'white'
 $(document).ready(function(){
-
     $('.nav-btn').css('color', 'white');
-
-    $('.field').click(function(){
-      $('#leftside').css('filter', 'brightness(0.5)');
-    });
-    $('#leftside').click(function(){
-      $('#leftside').css('filter', 'brightness(1)');
-    })
+    // 
+    // $('.field').click(function(){
+    //   $('#leftside').css('filter', 'brightness(0.5)');
+    // });
+    // $('#leftside').click(function(){
+    //   $('#leftside').css('filter', 'brightness(1)');
+    // })
 });
